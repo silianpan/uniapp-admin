cf761a15672322cfd8069b49d56486cf454d5125
liupan <1206284818@qq.com>
2019-11-06 01:34:31 +0800

    文件预览web-view增加缩放

--- pages/file/file-preview.vue
+++ pages/file/file-preview.vue
@@ -14,6 +14,15 @@
 				allUrl: ''
 			}
 		},
+		onReady() {
+			// #ifdef APP-PLUS
+			var currentWebview = this.$mp.page.$getAppWebview() //获取当前页面的webview对象
+			setTimeout(function() {
+				wv = currentWebview.children()[0]
+				wv.setStyle({scalable:true})
+			}, 1000); //如果是页面初始化调用时，需要延时一下
+			// #endif
+		},
 		onLoad(options) {
 			let fileUrl = encodeURIComponent(
 				globalConfig.baseUrl + '/api/attachment?name=' + options.name + '&url=' + options.url)
